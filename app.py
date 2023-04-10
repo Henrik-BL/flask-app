e91d3952efe2acbb7e173431fedffab43c2f8996
squaad99 <squaaad1993@gmail.com>
2023-04-10 02:01:01 +0200

    test

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ from flask import Flask
 from flask import render_template
 
 app = Flask(__name__)
-
+s
 
 @app.route("/")
 def hello_world():
-    return render_template("index.html")
+    return "Hello"
